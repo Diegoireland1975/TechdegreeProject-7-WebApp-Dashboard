22c116a35fa2b5572945dd7b816d73c143a42b0b
Diego Raffa <diego170975@gmail.com>
2020-07-29 09:49:25 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 const alertBanner = document.getElementById("alert");
-// create the HTML for the banner
+// create the HTML for the Banner
 
 alertBanner.innerHTML = `
 <div class="alert-banner">
-  <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
-  <p class="alert-banner-close">x</p>
+<p><strong>Alert:</strong></p>You have <strong>6</strong> overdue tasks to complete</p>
+<p class="alert-banner-close">x</p>
 </div>
 `;
-
 alertBanner.addEventListener("click", (e) => {
   const element = e.target;
-  ​if​ (element.classList.contains(​"alert-banner-close"​)) { 
-     alert.style.display = ​"none"
-    } 
+  if (element.classList.contains("alert-banner-close")) {
+    alert.style.display = "none";
+  }
 });
