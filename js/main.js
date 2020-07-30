4e5366e1808f4da3096f03c60e9fc0adb1833340
Diego Raffa <diego170975@gmail.com>
2020-07-30 23:44:53 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ popUp.addEventListener("click", () => {
 const alertBanner = document.getElementById("alert");
 alertBanner.innerHTML = `
 <div class="alert-banner">
-<p><strong>Alert:</strong></p>You have <strong>6</strong> overdue tasks to complete</p>
+<p><strong>Alert:</strong></p>You have <strong> 6 </strong> overdue tasks to complete</p>
 <p class="alert-banner-close">x</p>
 </div>
 `;
